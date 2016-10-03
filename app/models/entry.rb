e12ef99333f89066e86c5b789ae442a32cf6fe76
k-olejnicki <kamil.olejnicki@gmail.com>
2016-10-03 22:27:31 +0200

    change from ftp to dropbox

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -4,6 +4,12 @@ class Entry < ActiveRecord::Base
   has_many :comments
   belongs_to :user
   has_attached_file :image, styles: { normal: "900x300" }
+  :storage => :dropbox,
+      :dropbox_credentials => "#{Rails.root}/config/dropbox_config.yml",
+      :dropbox_options => {
+      :path => proc { |style| "#{style}/#{id}_#{image.original_filename}"},
+      :unique_filename => true},
+      :dropbox_visibility => 'public'
   validates_attachment :image, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
   def self.search(query)
     where("title like ?", "%#{query}%")
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,13 @@
 class User < ActiveRecord::Base
  has_secure_password
- has_attached_file :avatar, styles: { img: "64x64"}
- validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
  has_many :entries
  has_many :comments
+ has_attached_file :avatar, styles: { img: "64x64"},
+     :storage => :dropbox,
+     :dropbox_credentials => "#{Rails.root}/config/dropbox_config.yml",
+     :dropbox_options => {
+     :path => proc { |style| "#{style}/#{id}_#{image.original_filename}"},
+     :unique_filename => true},
+     :dropbox_visibility => 'public'
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 end
